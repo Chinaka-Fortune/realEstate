ff5c8eb0a56e321cda9f00db138ba47770fb6be2
BENEDICT <benidzy.bi@gmail.com>
2025-07-13 18:03:52 +0100

    rounded up static of our popular properties component

--- real_estate/src/home/homeComponents/OurPopularHomes.js
+++ real_estate/src/home/homeComponents/OurPopularHomes.js
@@ -1,17 +1,37 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useRef, useState } from "react";
 import OurPopularHomeComp from "./OurPopularHomeComp";
 
 import BedRoomDetach from "../homeImages/5 bedroom detached duplex for sale lekki lagos.jpg";
+import whiteMassion from "../homeImages/whitemassion.jpg"
+import bedRoomBQ from "../homeImages/Lovely 5bedroom duplex with 2 rooms BQ,.jpg"
+import luxuryHome from "../homeImages/Luxury Home.jpg"
+import modernHouse from "../homeImages/modern house.jpg"
+import bedRoomBeast from "../homeImages/The _Beast_ 5-Bedroom Detached Duplex_.jpg"
 import "../Home.css";
 
 const OurPopularHomes = () => {
+  const [degrees, setDegrees] = useState(0)
+  const RotatingBoxRef = useRef(null)
+
+
+  const handleprevClick = () =>{
+    setDegrees((prevDegrees)=> prevDegrees + 60)
+    RotatingBoxRef.current.style =`transform: perspective(1000px) rotateY(${degrees + 60}deg) ; `
+  }
+  const handleNextClick = () =>{
+    setDegrees((nextDegrees)=> nextDegrees - 60)
+    RotatingBoxRef.current.style =`transform: perspective(1000px) rotateY(${degrees - 60}deg); `
+  }
+
   return (
-    <div className="container-fluid mt-5">
-      <div className="row">
-        <h2 className="bg-danger text-center mb-4 fw-bolder">PORPULAR PROPERTIES</h2>
-        <div className=" ourComponentImgDiv d-flex justify-content-center gap-4">
-          <div className="  position-relative" style={{ "--i": 1 }}>
+    <div className="container-fluid  ourComponentContainerDivColor position-relative">
+      <div className="row d-flex justify-content-center align-items-center ">
+        <h2 className="text-white text-center mb-4 fw-bolder pt-3">POPULAR PROPERTIES</h2>
+        <div  className=" ourComponentImgDiv d-flex justify-content-center align-items-center" ref={RotatingBoxRef} >
+          
+          <span className="" style={{ "--i": 1 }}>
             <img
               src={BedRoomDetach}
               alt="5 bedroom detached duplex for sale lekki lagos"
@@ -22,10 +42,10 @@ const OurPopularHomes = () => {
             >
               Details <i bi bi-arrow></i>
             </button>
-          </div>
-          <div className="  position-relative" style={{ "--i": 2 }}>
+          </span>
+          <span className="  position-relativ" style={{ "--i": 2 }}>
             <img
-              src={BedRoomDetach}
+              src={bedRoomBeast}
               alt="5 bedroom detached duplex for sale lekki lagos"
             />
             <button
@@ -34,10 +54,10 @@ const OurPopularHomes = () => {
             >
               Details <i bi bi-arrow></i>
             </button>
-          </div>
-          <div className=" position-relative" style={{ "--i": 3 }}>
+          </span>
+          <span className=" position-relativ" style={{ "--i": 3 }}>
             <img
-              src={BedRoomDetach}
+              src={modernHouse}
               alt="5 bedroom detached duplex for sale lekki lagos"
             />
             <button
@@ -46,10 +66,10 @@ const OurPopularHomes = () => {
             >
               Details <i bi bi-arrow></i>
             </button>
-          </div>
-          <div className=" position-relative" style={{ "--i": 4 }}>
+          </span>
+          <span className=" position-relativ" style={{ "--i": 4 }}>
             <img
-              src={BedRoomDetach}
+              src={luxuryHome}
               alt="5 bedroom detached duplex for sale lekki lagos"
             />
             <button
@@ -58,10 +78,10 @@ const OurPopularHomes = () => {
             >
               Details <i bi bi-arrow></i>
             </button>
-          </div>
-          <div className=" position-relative" style={{ "--i": 5 }}>
+          </span>
+          <span className=" position-relativ" style={{ "--i": 5 }}>
             <img
-              src={BedRoomDetach}
+              src={bedRoomBQ}
               alt="5 bedroom detached duplex for sale lekki lagos"
             />
             <button
@@ -70,8 +90,25 @@ const OurPopularHomes = () => {
             >
               Details <i bi bi-arrow></i>
             </button>
-          </div>
+          </span>
+          <span className=" position-relativ" style={{ "--i": 6 }}>
+            <img
+              src={whiteMassion}
+              alt="5 bedroom detached duplex for sale lekki lagos"
+            />
+            <button
+              type="button"
+              className="btn rounded-5 ourComponentImgDivBtn "
+            >
+              Details <i bi bi-arrow></i>
+            </button>
+          </span>
         </div>
+      <div className="d-flex justify-content-center column-gap-5"> 
+          <button className="btn popularPrevBtn btn-primary rounded-pill px-4 border-0" onClick={handleprevClick} >Back</button>
+
+          <button className="btn popularNextBtn btn-primary rounded-5 rounded-pill px-4 border-0" onClick={handleNextClick} >Next</button>
+          </div>
       </div>
     </div>
   );
