64b94e658c763cce01bb452db3a6dda0e9269c27
BENEDICT <benidzy.bi@gmail.com>
2025-07-21 23:50:05 +0100

    new properties section

--- real_estate/src/newProperties/NewProperties.js
+++ real_estate/src/newProperties/NewProperties.js
@@ -1,18 +1,27 @@
-import React from 'react'
-import NewPropertiesComp from './NewPropertiesComp'
-import propertyDev from "./newPropImages/Property-Development-Fateck-4.jpg"
-import "./newProperties.css"
-import "../index.css"
+import React from "react";
+import NewPropertiesComp from "./NewPropertiesComp";
+import propertyDev from "./newPropImages/Property-Development-Fateck-4.jpg";
+import "./newProperties.css";
+import "../index.css";
 const NewProperties = () => {
   return (
-    <div className='container'>
-      <div className="row">
-        <div className="col-2">
-            <NewPropertiesComp propertyPix ={propertyDev} propertyText ="lorem5 gges ansj"/>
+    <div>
+      <img src={propertyDev} alt="" className="propertyBanner" />
+
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <div className="col-2 bb">
+            <img src={propertyDev} alt="" className="rounded" />
+            <div>
+              <p>location</p>
+              <p>agent</p>
+              
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewProperties
+export default NewProperties;
