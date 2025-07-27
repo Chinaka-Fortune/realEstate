6e31ee91a8570abd8c69171aa43d3daac041f49e
BENEDICT <benidzy.bi@gmail.com>
2025-07-27 23:42:03 +0100

    new proj

--- real_estate/src/newProperties/NewProperties.js
+++ real_estate/src/newProperties/NewProperties.js
@@ -27,8 +27,78 @@ const NewProperties = () => {
 
       <div className="container-fluid mt-5">
         <div className="row">
-          <div className="  d-flex justify-content-center align-items-center ">
-            <div className="col-3 b">
+          <div className=" d-flex flex-wrap newPropertyContaner mx-auto justify-content-between align-items-center row-gap-3">
+            <div className="col-3 ">
+              <div className="newPropertyImageDdivCard">
+                <div className="newPropertyImageDdiv">
+                  <img src={redHouse} alt="" />
+                </div>
+                <div className="content">
+                  <div className="price">
+                    <p className="fw-bolder rounded-2 priceAmount">
+                      ₦7,567,854,000
+                    </p>
+                    <p className="saleDetails text-white px-3 fw-bolder ms-3 mt-1 rounded-pill bg-succes">
+                      For Sale
+                    </p>
+                  </div>
+                  <div className="propertyinformation row lh-1">
+                    <div className="col-9 mt-1 lh-1">
+                      <p className=" text-white fw-bolder">4 Bedroom duplex</p>
+                      <div className="d-flex column-gap-3 lh-1">
+                        <p className=" fw-bold text-white">
+                          <FontAwesomeIcon
+                            icon={faBed}
+                            className="me-2 fw-bold"
+                          />
+                          7
+                        </p>
+                        <p className=" fw-bold text-white">
+                          <FontAwesomeIcon
+                            icon={faBath}
+                            className="me-2  fw-bold"
+                          />
+                          5
+                        </p>
+                        <p className=" fw-bold text-white">
+                          <i class="bi bi-arrows-fullscreen me-2  fw-bolder"></i>
+                          500sqm
+                        </p>
+                      </div>
+                      <div className="d-flex align-items-center column-gap-2">
+                        <div className="newPropertyAgentDiv">
+                          <img src={propertyDev} alt="" />
+                        </div>
+                        <h6 className="text-white">ziff Properties</h6>
+                      </div>
+                      <div className="mt-2 d-flex column-gap-3 align-items-center">
+                        <button className="px-3 rounded-pill bg-transparent text-white py-1 fw-bold">
+                          <em>View more Details</em>{" "}
+                          <i class="bi bi-arrow-right-circle"></i>
+                        </button>
+                      </div>
+                    </div>
+                    <div className="col-3 d-flex flex-column justify-content-center align-items-center row-gap-2">
+                      <button className="px-4 py-1  rounded-pill border-2 bg-transparent d-flex justify-content-center">
+                        <i class="bi bi-whatsapp text-success fw-bolder"></i>
+                      </button>
+                      <button className="rounded-pill px-4 py-1 border-2  bg-transparent text-white d-flex align-items-center">
+                        <i class="bi bi-telephone-outbound-fill text-success"></i>
+                      </button>
+
+                      <div onClick={handleIcon}>{favouriteColour}</div>
+                      <input
+                        type="checkbox"
+                        name=""
+                        id=""
+                        className="pointerCursorStyle"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-3 ">
               <div className="newPropertyImageDdivCard">
                 <div className="newPropertyImageDdiv">
                   <img src={redHouse} alt="" />
@@ -308,6 +378,7 @@ const NewProperties = () => {
                 </div>
               </div>
             </div>
+            
           </div>
         </div>
       </div>
