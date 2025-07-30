3839efe010e551fa1982f0d3d906d7045963e7b5
BENEDICT <benidzy.bi@gmail.com>
2025-07-30 23:45:37 +0100

    agent update

--- real_estate/src/agentFiles/AgentFile.js
+++ real_estate/src/agentFiles/AgentFile.js
@@ -1,12 +1,60 @@
-import React from 'react'
-import './agentFile.css'
-import '../index.css'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./agentFile.css";
+import "../index.css";
+import defaultEstate from "./agentFileImage/default-estate-agents-for-residential-rental-9.avif";
+
+import blackGuy from "./agentFileImage/images.jpg";
 const AgentFile = () => {
   return (
     <>
-      <h1>hello</h1>
-    </>
-  )
-}
+      <div className="searchAgents py-5 bg-white">
+        <div className="searchConAgent mt-4 d-flex align-items-center justify">
+          <input type="text" placeholder="search agent" />
+        </div>
+        <button></button>
+      </div>
 
-export default AgentFile
+      <div className="Agentcontainer mt-5 d-flex gap-4">
+        <div className="card">
+          <div className="imgDiv">
+            <img src={defaultEstate} alt="" />
+          </div>
+          <div className="content">
+            <h3>benedict <i bi bi-facebook></i></h3>
+            <h5>Lagos Nigeria</h5>
+            <div>
+              <NavLink to=''><i bi bi-twitter></i></NavLink>
+              <NavLink to=''></NavLink>
+              <NavLink to=''></NavLink>
+              <NavLink to=''></NavLink>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="imgDiv">
+            <img src={defaultEstate} alt="" />
+          </div>
+          <div className="content"></div>
+        </div>
+        <div className="card">
+          <div className="imgDiv">
+            <img src={defaultEstate} alt="" />
+          </div>
+          <div className="content">
+            <h3>benedict <i bi bi-facebook></i></h3>
+            <h5>Lagos Nigeria</h5>
+            <div>
+              <NavLink to=''><i bi bi-twitter></i></NavLink>
+              <NavLink to=''></NavLink>
+              <NavLink to=''></NavLink>
+              <NavLink to=''></NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AgentFile;
