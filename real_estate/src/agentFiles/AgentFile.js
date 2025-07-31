c86faa95f0281c03da84be717832a3d02f75594c
BENEDICT <benidzy.bi@gmail.com>
2025-07-31 23:55:20 +0100

    agent cards

--- real_estate/src/agentFiles/AgentFile.js
+++ real_estate/src/agentFiles/AgentFile.js
@@ -15,19 +15,22 @@ const AgentFile = () => {
         <button></button>
       </div>
 
-      <div className="Agentcontainer mt-5 d-flex gap-4">
+      <div className="Agentcontainer mt-5 d-flex ">
         <div className="card">
           <div className="imgDiv">
             <img src={defaultEstate} alt="" />
           </div>
           <div className="content">
-            <h3>benedict <i bi bi-facebook></i></h3>
-            <h5>Lagos Nigeria</h5>
-            <div>
-              <NavLink to=''><i bi bi-twitter></i></NavLink>
-              <NavLink to=''></NavLink>
-              <NavLink to=''></NavLink>
-              <NavLink to=''></NavLink>
+            <h3> <i class="bi bi-person-circle"></i> benedict <i bi bi-facebook></i></h3>
+            <h5><i class="bi bi-geo-alt-fill"></i> Lagos Nigeria</h5>
+            <button type="button" className=" rounded-pill px-4 py-1">Posted {16} property(ies)</button>
+            <div className="d-flex justify-content-center gap-3">
+              <NavLink to='' className="iconNavLink p-2 border border-secondary rounded-circle "><i class="bi bi-facebook fw-bolder"></i></NavLink>
+              <NavLink to='' className="iconNavLink p-2 border border-secondary rounded-circle "><i class="bi bi-whatsapp fw-bolder"></i></NavLink>
+              <NavLink to='' className="iconNavLink p-2 border border-secondary rounded-circle "><i class="bi bi-twitter-x fw-bolder"></i></NavLink>
+              <NavLink to='' className="iconNavLink p-2 border border-secondary rounded-circle "><i class="bi bi-instagram fw-bolder"></i></NavLink>
+              <NavLink to='' className="iconNavLink p-2 border border-secondary rounded-circle "><i class="bi bi-linkedin fw-bolder"></i></NavLink>
+              <NavLink to='' className="iconNavLink p-2 border border-secondary rounded-circle "><i class="bi bi-telegram fw-bolder"></i></NavLink>
             </div>
           </div>
         </div>
