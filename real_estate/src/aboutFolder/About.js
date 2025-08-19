12152f817fc33db1a3f024daef040ee8ec77a788
BENEDICT <benidzy.bi@gmail.com>
2025-08-19 23:02:05 +0100

    about section

--- real_estate/src/aboutFolder/About.js
+++ real_estate/src/aboutFolder/About.js
@@ -5,15 +5,16 @@ import blackHouse from "../home/homeImages/darkBuilding.jpg";
 const About = () => {
   return (
     <div className="container-fluid">
-      <div className="row ">
+      <div className="row g">
         <h3 className="text-center mt-4 mt-sm-0">ABOUT US</h3>
-        <div className="d-md-flex column-gap-3">
-          <div className="col-md-6 col-sm-12">
+        <div className="">
+        <div className="d-md-flex column-gap-5 align-items-center mb-4 aboutUsBigScreen mx-auto">
+          <div className="col-md-5 col-sm-12">
             <div className='aboutImageDiv'>
             <img src={blackHouse} alt="" className="" />
             </div>
           </div>
-          <div className="col-md-6 p-2">
+          <div className="col-md-5 ">
             <p className="">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae,
               odit cupiditate repellat non voluptatibus iure velit delectus unde
@@ -35,24 +36,25 @@ const About = () => {
         </div>
       </div>
 
-      <div className="row gap-2 d-flex justify-content-center">
-        <div className="col-5 py-3 bg-primary">
+      <div className="row gap-2 d-flex justify-content-center mb-3">
+        <div className="col-5 col-lg-2 py-3 bg-primary">
           <h3 className="text-center">4,567</h3>
           <p className="text-nowrap text-center fw-bold">Houses sold</p>
         </div>
-        <div className="col-5 py-3 bg-primary">
+        <div className="col-5 col-lg-2 bg-primary d-flex align-items-center align-items-center flex-column justify-content-center">
           <h3 className="text-center">44,034</h3>
           <p className="text-nowrap text-center fw-bold">Lands</p>
         </div>
-        <div className="col-5 py-3 bg-primary">
+        <div className="col-5 col-lg-2 py-3 bg-primary">
           <h3 className="text-center">1,203,004</h3>
           <p className="text-nowrap text-center fw-bold">Properties</p>
         </div>
-        <div className="col-5 py-3 bg-primary">
+        <div className="col-5 col-lg-2 py-3 bg-primary">
           <h3 className="text-center">4034</h3>
           <p className="text-nowrap text-center fw-bold">ShortLet rented</p>
         </div>
       </div>
+    </div>
     </div>
   );
 };
